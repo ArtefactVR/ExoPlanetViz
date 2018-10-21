df913df987e57f71299bb8096bc63b5c3d837954
MatrixInception <markingram2007@gmail.com>
2018-10-21 01:26:22 -0400

    Commit - Input Cubes 2

---
+++ Assets/SkyFade.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyFade : MonoBehaviour {
+    Material thisMat;
+    Color originalColor;
+
+
+	// Use this for initialization
+	void Start () {
+        thisMat = GetComponent<Renderer>().material;
+        originalColor = thisMat.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void FadeOut()
+    {
+        thisMat.SetColor("_Color", new Color(originalColor.r, originalColor.g, originalColor.b, originalColor.a * 0.5f));
+    }
+
+    public void FadeIn()
+    {
+        thisMat.SetColor("_Color", new Color(originalColor.r, originalColor.g, originalColor.b, originalColor.a));
+    }
+
+    public void ClickConfirmed()
+    {
+        Debug.Log("ClickConfirmed:"+Time.realtimeSinceStartup+" ");
+    }
+}
